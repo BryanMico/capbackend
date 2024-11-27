b1873a7a48c3e23d2fce9b779ed40235e90def30
BryanMico <imbryanmicobaril@gmail.com>
2024-11-27 22:55:28 +0800

    a

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
   const crypto = require('crypto'); // For generating verification code
   const jwt = require('jsonwebtoken');
   const corsconfig = {
-    origin: "*",
+    origin: "https://capbackend-liq2zxc5l-bryanmicos-projects.vercel.app/",
     credential: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   };
